42f33a6d26c2901645b738bc4bd5678a03b93ceb
Oksana Miazina <oksana.myazina@gmail.com>
2024-04-01 09:18:17 +0300

    feat: add BookDao

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("Test");
+
+        bookDao.create(book);
+
+        book.setTitle("Greystok");
+        bookDao.update(book);
+
+        System.out.println(bookDao.findById(1L));
+
+        bookDao.findAll().forEach(System.out::println);
+
+        bookDao.deleteById(1L);
     }
 }
